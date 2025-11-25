84d63181899dd5b8da82bd59b4af169bf0bd2bf8
Drew Tada <drew.tada@gmail.com>
2025-11-24 19:18:19 -0500

    more styling

--- app/[handle]/edit/page.tsx
+++ app/[handle]/edit/page.tsx
@@ -52,7 +52,7 @@ export default async function EditProfilePage({ params }: { params: Promise<{ ha
           <label>Links</label>
         </div>
         <ProfileLinksEditor initialLinks={profileLinks} />
-        <button type="submit" className="floating">Save</button>
+        <button type="submit" className="floating emboss">Save</button>
       </form>
       <p>
         <Link href={`/${profile.handle}`}>&larr; Profile</Link>
